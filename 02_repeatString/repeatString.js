e6f8c27b721c9f07f1ae1544dfebee21478ec746
santiago4343 <sansotdoz@gmail.com>
2024-04-18 13:11:46 -0500

    second

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+    let stringTimes = []
+    while (numTimes > 0) {
+        stringTimes.push(string)
+        numTimes--
+    }
+    return stringTimes.join('')
 };
-
 // Do not edit below this line
 module.exports = repeatString;
